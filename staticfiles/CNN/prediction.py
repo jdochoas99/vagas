f88903146ce7d736e151adc1045233cc5e3b392c
Juan David Ochoa Saavedra <jdochoas99@gmail.com>
2022-10-25 11:29:35 -0400

    vagas guardadas

--- staticfiles/CNN/prediction.py
+++ staticfiles/CNN/prediction.py
@@ -3,11 +3,10 @@ import pickle
 import cvzone
 import numpy as np
 import tensorflow as tf
-vcap = cv.VideoCapture(
-    "rtsp://teste:teste123@10.22.22.50:554/cam/realmonitor?channel=1&subtype=0")
+
 new_model = tf.keras.models.load_model('staticfiles/CNN/saved_model/my_model.h5')
 # vcap = cv.imread('test_images/20220616_094946.png')
-ret, frame = vcap.read()
+
 # frame = vcap
 
 with open('staticfiles/CNN/CarParkPos', 'rb') as f:
@@ -19,6 +18,9 @@ a = xy.values.tolist()
 
 
 def mkprediction():
+    vcap = cv.VideoCapture(
+        "rtsp://teste:teste123@10.22.22.50:554/cam/realmonitor?channel=1&subtype=0")
+    ret, frame = vcap.read()
     predresult = []
     for count, pos in enumerate(posList):
         coordinates = np.array(pos)
