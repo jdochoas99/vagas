4394a7fb3a1338162219af5e676578ca0bb3b56a
Juan David Ochoa Saavedra <jdochoas99@gmail.com>
2022-11-01 08:42:17 -0400

    add gitignore

--- staticfiles/CNN/predict.py
+++ staticfiles/CNN/predict.py
@@ -8,10 +8,10 @@ def finalpredict():
         t.write('{% block rect %}')
         for count, _ in enumerate(a):
             if _[1] == 1:
-                print(f'<rect x=" {_[0][0]} " y="{_[0][1]}" style="fill:rgb(255, 0, 0)" opacity="1" width="40" height="90" />')
+                #print(f'<rect x=" {_[0][0]} " y="{_[0][1]}" style="fill:rgb(255, 0, 0)" opacity="1" width="40" height="90" />')
                 t.write(f'<rect x=" {_[0][0]} " y="{_[0][1]}" style="fill:rgb(255, 0, 0)" opacity="1" width="40" height="90" />')
             else:
-                print(f'<rect x=" {_[0][0]} " y=" {_[0][1]}" style="fill:rgb(0, 255, 0)" opacity="1" width="40" height="90" />')
+                #print(f'<rect x=" {_[0][0]} " y=" {_[0][1]}" style="fill:rgb(0, 255, 0)" opacity="1" width="40" height="90" />')
                 t.write(f'<rect x=" {_[0][0]} " y=" {_[0][1]}" style="fill:rgb(0, 255, 0)" opacity="1" width="40" height="90" />')
         t.write('{% endblock rect %}')
         return 1
